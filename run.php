47db7ac2964e21d3ff81a4b7c2509f8b34270539
Ivan Ramljak <ivanoramljak@gmail.com>
2020-07-14 16:08:53 +0200

    gitignore + cleanup run.php

--- run.php
+++ run.php
@@ -7,22 +7,6 @@ use csip\Activator;
 use csip\Deactivator;
 use csip\PluginInit;
 
-// use Carbon_Fields\Container;
-// use Carbon_Fields\Field;
-
-// add_action( 'carbon_fields_register_fields', 'crb_attach_theme_options' );
-// function crb_attach_theme_options() {
-//     Container::make( 'theme_options', __( 'Invoice Plugin Options', 'crb' ) )
-//         ->add_fields( array(
-//             Field::make( 'text', 'crb_text', 'Text Field' ),
-//         ) );
-// }
-
-// add_action( 'after_setup_theme', 'crb_load' );
-// function crb_load() {
-//     // require_once( 'vendor/autoload.php' );
-//     \Carbon_Fields\Carbon_Fields::boot();
-// }
 // If this file is called directly, abort.
 defined( 'WPINC' ) || die;
 
