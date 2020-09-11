087d1e2479abf0fae6140d309aca0e6a9bc39b05
Ivan Ramljak <ivanoramljak@gmail.com>
2020-09-11 17:01:03 +0200

    add mising "text domain"

--- includes/admin/fields/Invoice.php
+++ includes/admin/fields/Invoice.php
@@ -6,22 +6,20 @@ use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
 // Exit if accessed directly
-defined('WPINC') || die;
+defined( 'WPINC' ) || die;
 
 
 /**
  * Class containing fields for the Invoice post-type
-
  */
-class Invoice
-{
+class Invoice {
+
 
 	/**
 	 * Load all custom field metaboxes for the Invoice post-type
 	 */
-	public static function load()
-	{
-		self::fields_general();
+	public static function load() {
+		 self::fields_general();
 		self::fields_items();
 		self::fields_note();
 	}
@@ -30,96 +28,107 @@ class Invoice
 	/**
 	 * Create general fields
 	 */
-	public static function fields_general()
-	{
-		Container::make('post_meta', __('General', PLUGIN_TEXT_DOMAIN))
-			->where('post_type', '=', 'invoice')
-			->add_fields(array(
-				Field::make('text', 'inv_number', __('Invoice number', PLUGIN_TEXT_DOMAIN))
-					->set_attribute('readOnly', true)
-					->set_default_value(\csip\admin\Helpers::get_invoice_number())
-					->set_classes('span-4 inv-number'),
-				Field::make('select', 'inv_client', __('Client', PLUGIN_TEXT_DOMAIN))
-					->set_options(\csip\admin\Helpers::get_clients())
-					->set_classes('span-4 inv-client csip-select2'),
-				Field::make('select', 'inv_status', __('Invoice Status', PLUGIN_TEXT_DOMAIN))
-					->set_options(array(
-						'' => __('-- Please Select'),
-						'inv_outstanding' => 'Outstanding',
-						'inv_paid' => 'Paid',
-						'inv_partially_paid' => 'Partially Paid',
-					))
-					->set_classes('span-4 inv-status csip-select2'),
-				Field::make('date', 'inv_date', __('Invoice Date', PLUGIN_TEXT_DOMAIN))
-					->set_classes('span-4 inv-date'),
-				Field::make('number', 'inv_net_period', __('Net', PLUGIN_TEXT_DOMAIN))
-					->set_min(0)
-					->set_classes('span-4 inv-net-period')
-					->set_help_text('Days until the payment is due'),
-				Field::make('date', 'inv_due_date', __('Invoice Due Date', PLUGIN_TEXT_DOMAIN))
-					->set_classes('span-4 inv-due-date'),
-				Field::make('select', 'inv_payment_account', __('Payment Account', PLUGIN_TEXT_DOMAIN))
-					->set_options(\csip\admin\Helpers::get_accounts())
-					->set_classes('inv-payment-account csip-select2'),
-			));
+	public static function fields_general() {
+		Container::make( 'post_meta', __( 'General', PLUGIN_TEXT_DOMAIN ) )
+			->where( 'post_type', '=', 'invoice' )
+			->add_fields(
+				array(
+					Field::make( 'text', 'inv_number', __( 'Invoice number', PLUGIN_TEXT_DOMAIN ) )
+						->set_attribute( 'readOnly', true )
+						->set_default_value( \csip\admin\Helpers::get_invoice_number() )
+						->set_classes( 'span-4 inv-number' ),
+					Field::make( 'select', 'inv_client', __( 'Client', PLUGIN_TEXT_DOMAIN ) )
+						->set_options( \csip\admin\Helpers::get_clients() )
+						->set_classes( 'span-4 inv-client csip-select2' ),
+					Field::make( 'select', 'inv_status', __( 'Invoice Status', PLUGIN_TEXT_DOMAIN ) )
+					->set_options(
+						array(
+							''                   => __( '-- Please Select', PLUGIN_TEXT_DOMAIN),
+							'inv_outstanding'    => 'Outstanding',
+							'inv_paid'           => 'Paid',
+							'inv_partially_paid' => 'Partially Paid',
+						)
+					)
+					->set_classes( 'span-4 inv-status csip-select2' ),
+					Field::make( 'date', 'inv_date', __( 'Invoice Date', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'span-4 inv-date' ),
+					Field::make( 'number', 'inv_net_period', __( 'Net', PLUGIN_TEXT_DOMAIN ) )
+						->set_min( 0 )
+						->set_classes( 'span-4 inv-net-period' )
+						->set_help_text( 'Days until the payment is due' ),
+					Field::make( 'date', 'inv_due_date', __( 'Invoice Due Date', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'span-4 inv-due-date' ),
+					Field::make( 'select', 'inv_payment_account', __( 'Payment Account', PLUGIN_TEXT_DOMAIN ) )
+						->set_options( \csip\admin\Helpers::get_accounts() )
+						->set_classes( 'inv-payment-account csip-select2' ),
+				)
+			);
 	}
 
 
 	/**
 	 * Create item repeater fields
 	 */
-	public static function fields_items()
-	{
-		Container::make('post_meta', __('Items list', PLUGIN_TEXT_DOMAIN))
-			->where('post_type', '=', 'invoice')
-			->add_fields(array(
-				Field::make('complex', 'inv_items', __('Items', PLUGIN_TEXT_DOMAIN))
-					->setup_labels( array(
-						'plural_name' => 'Items',
-						'singular_name' => 'Item',
-					))
-					->add_fields(array(
-						Field::make('text', 'inv_item_title', __('Title', PLUGIN_TEXT_DOMAIN))
-							->set_classes('inv-item-title'),
-						Field::make('textarea', 'inv_item_description', __('Description', PLUGIN_TEXT_DOMAIN))
-							->set_rows(2)
-							->set_classes('inv-item-description'),
-						Field::make('number', 'inv_item_quantity', __('Quantity', PLUGIN_TEXT_DOMAIN))
-							->set_min(0)
-							->set_classes('span-item-col inv-item-quantity'),
-						Field::make('text', 'inv_item_um', __('Unit', PLUGIN_TEXT_DOMAIN))
-							->set_classes('span-item-col inv-item-um'),
-						Field::make('number', 'inv_item_rate', __('Rate', PLUGIN_TEXT_DOMAIN))							->set_min(0)
-							->set_classes('span-item-col inv-item-rate'),
-						Field::make('number', 'inv_item_discount', __('Discount (%)'))
-							->set_min(0)
-							->set_max(100)
-							->set_default_value(0)
-							->set_classes('span-item-col inv-item-discount'),
-						Field::make('text', 'inv_item_amount', __('Amount', PLUGIN_TEXT_DOMAIN))
-							->set_attribute('readOnly', true)
-							->set_classes('span-item-col inv-item-amount'),
-					))
-					->set_header_template( '
+	public static function fields_items() {
+		 Container::make( 'post_meta', __( 'Items list', PLUGIN_TEXT_DOMAIN ) )
+			->where( 'post_type', '=', 'invoice' )
+			->add_fields(
+				array(
+					Field::make( 'complex', 'inv_items', __( 'Items', PLUGIN_TEXT_DOMAIN ) )
+					->setup_labels(
+						array(
+							'plural_name'   => 'Items',
+							'singular_name' => 'Item',
+						)
+					)
+					->add_fields(
+						array(
+							Field::make( 'text', 'inv_item_title', __( 'Title', PLUGIN_TEXT_DOMAIN ) )
+								->set_classes( 'inv-item-title' ),
+							Field::make( 'textarea', 'inv_item_description', __( 'Description', PLUGIN_TEXT_DOMAIN ) )
+								->set_rows( 2 )
+								->set_classes( 'inv-item-description' ),
+							Field::make( 'number', 'inv_item_quantity', __( 'Quantity', PLUGIN_TEXT_DOMAIN ) )
+								->set_min( 0 )
+								->set_classes( 'span-item-col inv-item-quantity' ),
+							Field::make( 'text', 'inv_item_um', __( 'Unit', PLUGIN_TEXT_DOMAIN ) )
+								->set_classes( 'span-item-col inv-item-um' ),
+							Field::make( 'number', 'inv_item_rate', __( 'Rate', PLUGIN_TEXT_DOMAIN ) )->set_min( 0 )
+								->set_classes( 'span-item-col inv-item-rate' ),
+							Field::make( 'number', 'inv_item_discount', __( 'Discount (%)', PLUGIN_TEXT_DOMAIN ) )
+								->set_min( 0 )
+								->set_max( 100 )
+								->set_default_value( 0 )
+								->set_classes( 'span-item-col inv-item-discount' ),
+							Field::make( 'text', 'inv_item_amount', __( 'Amount', PLUGIN_TEXT_DOMAIN ) )
+								->set_attribute( 'readOnly', true )
+								->set_classes( 'span-item-col inv-item-amount' ),
+						)
+					)
+					->set_header_template(
+						'
 						<% if (inv_item_title) { %>
 							<%- inv_item_title %>
 						<% } %>
-					' )
-					->set_classes('cf-invoice-items'),
-			));
+					'
+					)
+					->set_classes( 'cf-invoice-items' ),
+				)
+			);
 	}
 
 
 	/**
 	 * Create note fields
 	 */
-	public static function fields_note()
-	{
-		Container::make('post_meta', __('Note', PLUGIN_TEXT_DOMAIN))
-			->where('post_type', '=', 'invoice')
-			->add_fields(array(
-				Field::make('textarea', 'inv_comment', __('Comment', PLUGIN_TEXT_DOMAIN))
-					->set_classes('inv-comment'),
-			));
+	public static function fields_note() {
+		Container::make( 'post_meta', __( 'Note', PLUGIN_TEXT_DOMAIN ) )
+			->where( 'post_type', '=', 'invoice' )
+			->add_fields(
+				array(
+					Field::make( 'textarea', 'inv_comment', __( 'Comment', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'inv-comment' ),
+				)
+			);
 	}
 }
--- includes/admin/fields/Options.php
+++ includes/admin/fields/Options.php
@@ -6,126 +6,152 @@ use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
 // Exit if accessed directly
-defined('WPINC') || die;
+defined( 'WPINC' ) || die;
 
 
 /**
  * Class containing fields for the Plugin options page.
- *
  */
-class Options
-{
+class Options {
+
 
 	/**
 	 * Load all custom field metaboxes for Plugin Options
 	 */
-	public static function load()
-	{
-		self::fields_company();
+	public static function load() {
+		 self::fields_company();
 	}
 
 	/**
 	 * Create fields for Company details
 	 */
-	public static function fields_company()
-	{
-		$allowed_tags_info = __('The following HTML tags are allowed: ', PLUGIN_TEXT_DOMAIN) . '&lt;b&gt;, &lt;i&gt;';
+	public static function fields_company() {
+		$allowed_tags_info = __( 'The following HTML tags are allowed: ', PLUGIN_TEXT_DOMAIN ) . '&lt;b&gt;, &lt;i&gt;';
 
-		Container::make('theme_options', __('Company Info', PLUGIN_TEXT_DOMAIN))
-			->set_page_menu_title( __('Invoice Plugin', PLUGIN_TEXT_DOMAIN) )
-			->add_tab(__('Branding', PLUGIN_TEXT_DOMAIN), array(
-				Field::make('text', 'csip_company_name', __('Company Name', PLUGIN_TEXT_DOMAIN))
-					->set_classes('csip-company-name'),
-				Field::make('text', 'csip_company_web', __('Website', PLUGIN_TEXT_DOMAIN))
-					->set_classes('csip-company-web'),
-				Field::make('image', 'csip_company_logo', __('Logo', PLUGIN_TEXT_DOMAIN))
-					->set_help_text('An image of your company logo for the Invoice header.')
-					->set_classes('span-6 csip-company-logo')->set_value_type( 'url' ),
-				Field::make('image', 'csip_company_signature', __('Signature', PLUGIN_TEXT_DOMAIN))
-					->set_help_text('A .png image of your signature if you want to insert it into Invoices.')
-					->set_classes('span-6 csip-company-signature')
-					->set_value_type( 'url' ),
-			))
-			->add_tab(__('Address', PLUGIN_TEXT_DOMAIN), array(
-				Field::make('text', 'csip_company_address_1', __('Address 1'))
-					->set_classes('span-6 csip-company-address-1'),
-				Field::make('text', 'csip_company_address_2', __('Address 2'))
-					->set_classes('span-6 csip-company-address-2'),
-				Field::make('text', 'csip_company_city', __('City', PLUGIN_TEXT_DOMAIN))
-					->set_classes('span-6 csip-company-city'),
-				Field::make('text', 'csip_company_zip', __('Zip Code', PLUGIN_TEXT_DOMAIN))
-					->set_classes('span-6 csip-company-zip'),
-				Field::make('select', 'csip_company_country', __('Country', PLUGIN_TEXT_DOMAIN))
-					->set_options(\csip\admin\Helpers::get_countries())
-					->set_classes('span-6 csip-company-country csip-select2'),
-				Field::make('select', 'csip_company_state', __('State', PLUGIN_TEXT_DOMAIN))
-					->set_options(\csip\admin\Helpers::get_states())
-					->set_classes('span-6 csip-company-state csip-select2'),
-			))
-			->add_tab(__('Contact', PLUGIN_TEXT_DOMAIN), array(
-				Field::make('text', 'csip_company_email', __('Email', PLUGIN_TEXT_DOMAIN))
-					->set_attribute('pattern', '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
-					->set_classes('csip-company-email'),
-				Field::make('text', 'csip_company_phone', __('Phone number', PLUGIN_TEXT_DOMAIN))
-					->set_classes('csip-company-phone'),
-			))
-			->add_tab(__('Legal', PLUGIN_TEXT_DOMAIN), array(
-				Field::make('text', 'csip_company_id', __('Company ID', PLUGIN_TEXT_DOMAIN))
-					->set_classes('csip-company-id'),
-				Field::make('select', 'csip_company_vatreg', __('VAT registered', PLUGIN_TEXT_DOMAIN))
-					->set_options(array(
-						'0' => 'No',
-						'1' => 'Yes',
-					))
-					->set_classes('span-6 csip-company-vatreg'),
-				Field::make('text', 'csip_company_vatid', __('VAT ID', PLUGIN_TEXT_DOMAIN))
-					->set_conditional_logic(array(
+		Container::make( 'theme_options', __( 'Company Info', PLUGIN_TEXT_DOMAIN ) )
+			->set_page_menu_title( __( 'Invoice Plugin', PLUGIN_TEXT_DOMAIN ) )
+			->add_tab(
+				__( 'Branding', PLUGIN_TEXT_DOMAIN ),
+				array(
+					Field::make( 'text', 'csip_company_name', __( 'Company Name', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'csip-company-name' ),
+					Field::make( 'text', 'csip_company_web', __( 'Website', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'csip-company-web' ),
+					Field::make( 'image', 'csip_company_logo', __( 'Logo', PLUGIN_TEXT_DOMAIN ) )
+						->set_help_text( __( 'An image of your company logo for the Invoice header.', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'span-6 csip-company-logo' )->set_value_type( 'url' ),
+					Field::make( 'image', 'csip_company_signature', __( 'Signature', PLUGIN_TEXT_DOMAIN ) )
+						->set_help_text( __( 'A .png image of your signature if you want to insert it into Invoices.', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'span-6 csip-company-signature' )
+						->set_value_type( 'url' ),
+				)
+			)
+			->add_tab(
+				__( 'Address', PLUGIN_TEXT_DOMAIN ),
+				array(
+					Field::make( 'text', 'csip_company_address_1', __( 'Address 1', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'span-6 csip-company-address-1' ),
+					Field::make( 'text', 'csip_company_address_2', __( 'Address 2', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'span-6 csip-company-address-2' ),
+					Field::make( 'text', 'csip_company_city', __( 'City', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'span-6 csip-company-city' ),
+					Field::make( 'text', 'csip_company_zip', __( 'Zip Code', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'span-6 csip-company-zip' ),
+					Field::make( 'select', 'csip_company_country', __( 'Country', PLUGIN_TEXT_DOMAIN ) )
+						->set_options( \csip\admin\Helpers::get_countries() )
+						->set_classes( 'span-6 csip-company-country csip-select2' ),
+					Field::make( 'select', 'csip_company_state', __( 'State', PLUGIN_TEXT_DOMAIN ) )
+						->set_options( \csip\admin\Helpers::get_states() )
+						->set_classes( 'span-6 csip-company-state csip-select2' ),
+				)
+			)
+			->add_tab(
+				__( 'Contact', PLUGIN_TEXT_DOMAIN ),
+				array(
+					Field::make( 'text', 'csip_company_email', __( 'Email', PLUGIN_TEXT_DOMAIN ) )
+						->set_attribute( 'pattern', '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$' )
+						->set_classes( 'csip-company-email' ),
+					Field::make( 'text', 'csip_company_phone', __( 'Phone number', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'csip-company-phone' ),
+				)
+			)
+			->add_tab(
+				__( 'Legal', PLUGIN_TEXT_DOMAIN ),
+				array(
+					Field::make( 'text', 'csip_company_id', __( 'Company ID', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'csip-company-id' ),
+					Field::make( 'select', 'csip_company_vatreg', __( 'VAT registered', PLUGIN_TEXT_DOMAIN ) )
+					->set_options(
 						array(
-							'field' => 'csip_company_vatreg',
-							'value' => '1',
+							'0' => 'No',
+							'1' => 'Yes',
 						)
-					))
-					->set_classes('span-6 csip-company-vatid'),
-			))
-			->add_tab(__('Bank Details', PLUGIN_TEXT_DOMAIN), array(
-				Field::make('complex', 'csip_company_bank_accounts', __('Accounts', PLUGIN_TEXT_DOMAIN))
-					->setup_labels( array(
-						'plural_name' => 'Accounts',
-						'singular_name' => 'Account',
-					))
-					->add_fields('account', array(
-						Field::make('text', 'csip_conpany_account_name', __('Name', PLUGIN_TEXT_DOMAIN))
-							->set_classes('csip-company-account-name'),
-						Field::make('textarea', 'csip_company_account_details', __('Account Details', PLUGIN_TEXT_DOMAIN))
-							->set_classes('span-6 csip-company-account-details')
-							->set_help_text($allowed_tags_info)
-							->set_rows( 8 ),
-						Field::make('textarea', 'csip_company_account_details_other', __('Other details e.g. Bank Address', PLUGIN_TEXT_DOMAIN))
-							->set_classes('span-6 csip-company-account-details-other')
-							->set_help_text($allowed_tags_info)
-							->set_rows( 8 ),
-					))
-					->set_header_template( '
+					)
+					->set_classes( 'span-6 csip-company-vatreg' ),
+					Field::make( 'text', 'csip_company_vatid', __( 'VAT ID', PLUGIN_TEXT_DOMAIN ) )
+					->set_conditional_logic(
+						array(
+							array(
+								'field' => 'csip_company_vatreg',
+								'value' => '1',
+							),
+						)
+					)
+					->set_classes( 'span-6 csip-company-vatid' ),
+				)
+			)
+			->add_tab(
+				__( 'Bank Details', PLUGIN_TEXT_DOMAIN ),
+				array(
+					Field::make( 'complex', 'csip_company_bank_accounts', __( 'Accounts', PLUGIN_TEXT_DOMAIN ) )
+					->setup_labels(
+						array(
+							'plural_name'   => 'Accounts',
+							'singular_name' => 'Account',
+						)
+					)
+					->add_fields(
+						'account',
+						array(
+							Field::make( 'text', 'csip_conpany_account_name', __( 'Name', PLUGIN_TEXT_DOMAIN ) )
+								->set_classes( 'csip-company-account-name' ),
+							Field::make( 'textarea', 'csip_company_account_details', __( 'Account Details', PLUGIN_TEXT_DOMAIN ) )
+								->set_classes( 'span-6 csip-company-account-details' )
+								->set_help_text( $allowed_tags_info )
+								->set_rows( 8 ),
+							Field::make( 'textarea', 'csip_company_account_details_other', __( 'Other details e.g. Bank Address', PLUGIN_TEXT_DOMAIN ) )
+								->set_classes( 'span-6 csip-company-account-details-other' )
+								->set_help_text( $allowed_tags_info )
+								->set_rows( 8 ),
+						)
+					)
+					->set_header_template(
+						'
 						<% if (csip_conpany_account_name) { %>
 							<%- csip_conpany_account_name %>
 						<% } %>
-					' )
-			))
-			->add_tab(__('Invoice Options', PLUGIN_TEXT_DOMAIN), array(
-				Field::make('text', 'csip_company_prefix', __('Invoice Prefix', PLUGIN_TEXT_DOMAIN))
-					->set_attribute('maxLength', 4)
-					->set_help_text('If needed you can prefix the invoice number with up to 4 characters in the print version of the invoice.')
-					->set_classes('span-6 csip-company-prefix'),
-				Field::make('number', 'csip_company_nin', __('Next Invoice Number', PLUGIN_TEXT_DOMAIN))
-					->set_help_text('This will be the next Invoice number, change this only if you need to reset it.')
-					->set_default_value(0)
-					->set_classes('span-6 csip-company-nin'),
-				Field::make('textarea', 'csip_company_terms', __('Terms & Conditions'))
-					->set_classes('csip-company-terms'),
-				Field::make('textarea', 'csip_company_note', __('Note', PLUGIN_TEXT_DOMAIN))
-					->set_classes('csip-company-dis'),
-				Field::make('text', 'csip_company_footertext', __('Footer Text', PLUGIN_TEXT_DOMAIN))
-					->set_classes('csip-company-footertext'),
-			));
+					'
+					),
+				)
+			)
+			->add_tab(
+				__( 'Invoice Options', PLUGIN_TEXT_DOMAIN ),
+				array(
+					Field::make( 'text', 'csip_company_prefix', __( 'Invoice Prefix', PLUGIN_TEXT_DOMAIN ) )
+						->set_attribute( 'maxLength', 4 )
+						->set_help_text( __( 'If needed you can prefix the invoice number with up to 4 characters in the print version of the invoice.', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'span-6 csip-company-prefix' ),
+					Field::make( 'number', 'csip_company_nin', __( 'Next Invoice Number', PLUGIN_TEXT_DOMAIN ) )
+						->set_help_text( __( 'This will be the next Invoice number, change this only if you need to reset it.', PLUGIN_TEXT_DOMAIN ) )
+						->set_default_value( 0 )
+						->set_classes( 'span-6 csip-company-nin' ),
+					Field::make( 'textarea', 'csip_company_terms', __( 'Terms & Conditions', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'csip-company-terms' ),
+					Field::make( 'textarea', 'csip_company_note', __( 'Note', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'csip-company-dis' ),
+					Field::make( 'text', 'csip_company_footertext', __( 'Footer Text', PLUGIN_TEXT_DOMAIN ) )
+						->set_classes( 'csip-company-footertext' ),
+				)
+			);
 	}
 }
